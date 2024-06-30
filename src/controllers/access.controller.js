b652216ebd75112e5359b70278da409d7eeff28d
minatisleeping <minatt2002@gmail.com>
2024-06-30 11:15:43 +0700

    Make ur API res use class..

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,9 +1,22 @@
 'use strict'
+const { CREATED } = require('../core/success.response')
 const AccessService = require('../services/access.service')
 const { StatusCodes } = require('http-status-codes')
 class AccessController {
   signUp = async (req, res, next) => {
-    return res.status(StatusCodes.CREATED).json(await AccessService.signUp(req.body))
+    // new CREATED({
+    //   message: 'Registered OK!',
+    //   metaData: await AccessService.signUp(req.body)
+    // }).send(res)
+
+    const result = await AccessService.signUp(req.body)
+    return res.status(StatusCodes.CREATED).json({
+      message: 'Registered OK!',
+      result,
+      options: {
+        limit: 10
+      }
+    })
   }
 }
 
---
+++ src/core/success.response.js
@@ -0,0 +1,36 @@
+'use strict'
+const { StatusCodes } = require('http-status-codes')
+
+const ReasonStatusCode = {
+  CREATED: 'Created ^_^',
+  OK: 'Success ^_^',
+}
+
+class SuccessResponse {
+  constructor({ message, statusCode = StatusCodes.OK, reasonStatusCode = ReassonStatusCode.OK, metaData = {} }) {
+    this.message = message ? message : reasonStatusCode
+    this.statusCode = statusCode
+    this.metaData = metaData
+  }
+
+  send(res, headers = {}) {
+    return res.status(this.status).json(this)
+  }
+}
+
+class OK extends SuccessResponse {
+  constructor({ message, metaData }) {
+    super({ message, metaData })
+  }
+}
+
+class CREATED extends SuccessResponse {
+  constructor({ options = {}, message, statusCode = StatusCodes.CREATED, reasonStatusCode = ReasonStatusCode.CREATED, metaData }) {
+    super({ message, statusCode, reasonStatusCode, metaData })
+    this.options = options
+  }
+}
+
+module.exports = {
+  OK, CREATED
+}
