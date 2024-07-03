8a5e084c5593feaa2763b36834b5cd1191cbaace
minatisleeping <minatt2002@gmail.com>
2024-07-03 12:12:56 +0700

    Design api service product combine Factory and Stategy..

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,11 +1,24 @@
 'use strict'
 
 const ProductService = require('../services/product.service')
+const ProductServiceV2 = require('../services/product.service.xxx')
 const { StatusCodes } = require('http-status-codes')
 
 class ProductController {
   createProduct = async (req, res, next) => {
     const result = await ProductService.createProduct(req.body.product_type, {
+      ...req.body,
+      product_shop: req.user.userId
+    })
+
+    return res.status(StatusCodes.OK).json({
+      message: 'Create new Product success!',
+      metaData: result
+    })
+  }
+
+  createProductV2 = async (req, res, next) => {
+    const result = await ProductServiceV2.createProduct(req.body.product_type, {
       ...req.body,
       product_shop: req.user.userId
     })
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -6,61 +6,42 @@ const DOCUMENT_NAME = 'Product'
 const COLLECTION_NAME = 'Products'
 
 const productSchema = new Schema({
-  product_name: {
-    type: String, required: true,
-    // trim: true, maxLength: 150
-  },
-  product_thumb: {
-    type: String, required: true,
-    // unique: true, trim: true
-  },
+  product_name:        { type: String, required: true },
+  product_thumb:       { type: String, required: true },
   product_description: { type: String },
-  product_price: { type: Number, required: true },
-  product_quantity: { type: Number, required: true },
-  product_type: {
-    type: String, required: true,
-    enum: ['Electronics', 'Clothing', 'Furniture']
+  product_price:       { type: Number, required: true },
+  product_quantity:    { type: Number, required: true },
+  product_type:        { type: String, required: true,
+                          enum: ['Electronics', 'Clothing', 'Furniture']
   },
-  product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
-  product_attributes: { type: Schema.Types.Mixed, required: true }
-}, {
-  collection: COLLECTION_NAME,
-  timestamps: true
-})
+  product_shop:        { type: Schema.Types.ObjectId, ref: 'Shop' },
+  product_attributes:  { type: Schema.Types.Mixed, required: true }
+}, { collection: COLLECTION_NAME, timestamps: true })
 
 const clothingSchema = new Schema({
-  brand: { type: String, required: true },
-  size: String,
-  material: String,
+  brand:        { type: String, required: true },
+  size:           String,
+  material:       String,
   product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' }
-}, {
-  collection: 'Clothes',
-  timestamps: true
-})
+}, { collection: 'Clothes', timestamps: true })
 
 const electronicsSchema = new Schema({
   manufacturer: { type: String, required: true },
-  model: String,
-  color: String,
+  model:          String,
+  color:          String,
   product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' }
-}, {
-  collection: 'Electronics',
-  timestamps: true
-})
+}, { collection: 'Electronics', timestamps: true })
 
 const furnitureSchema  = new Schema({
-  brand: { type: String, required: true },
-  size: String,
-  material: String,
+  brand:        { type: String, required: true },
+  size:           String,
+  material:       String,
   product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' }
-}, {
-  collection: 'Furniture',
-  timestamps: true
-})
+}, { collection: 'Furniture', timestamps: true })
 
 module.exports = {
-  product: model(DOCUMENT_NAME, productSchema),
+  product:    model(DOCUMENT_NAME, productSchema),
   electronic: model('Electronics', electronicsSchema),
-  clothing: model('Clothing', clothingSchema),
-  furniture: model('Furniture', furnitureSchema)
+  clothing:   model('Clothing',    clothingSchema),
+  furniture:  model('Furniture',   furnitureSchema)
 }
