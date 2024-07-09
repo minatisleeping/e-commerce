22106a129b73348e37c02306ba6ff65ce69dd907
minatisleeping <minatt2002@gmail.com>
2024-07-09 15:26:41 +0700

    isDraft, is-unPublish and search siucapvippro..

--- src/routes/product/index.js
+++ src/routes/product/index.js
@@ -6,13 +6,18 @@ const router = express.Router()
 const { wrapAsync } = require('../../helpers/wrapAsync')
 const { authenticationV2 } = require('../../auth/authUtils')
 
+router.get('/search/:keySearch', wrapAsync(ProductController.getListSearchProduct))
+
 // authentication
 router.use(authenticationV2)
 
 //logout
 router.post('', wrapAsync(ProductController.createProductV2))
+router.post('/publish/:id', wrapAsync(ProductController.publishProductByShop))
+router.post('/unpublish/:id', wrapAsync(ProductController.unPublishProductByShop))
 
 //query
 router.get('/drafts/all', wrapAsync(ProductController.getAllDraftsForShop))
+router.get('/published/all', wrapAsync(ProductController.getAllPublishForShop))
 
 module.exports = router
